e80173c8f343a4a9859952fd97e0cf2bf9233d8c
LeoSuarezz <leonardosuarezok@gmail.com>
2024-06-09 08:05:56 -0300

    Modificaciiones basicas en Vistas y Controladores de MESAS: 1 - Edición de titulos 2 -ajuste de nombre y vista de columnas 3- ajuste de datos unicos para Estado 4 - Validaciones de no permitir crear mesas con mismo num en mismo resto

--- obj/Debug/net8.0/ObligatorioProgram3.AssemblyInfo.cs
+++ obj/Debug/net8.0/ObligatorioProgram3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioProgram3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deefcd508d704d92360b68dd3ed73ef7e3f9406d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioProgram3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioProgram3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
