ac58f7904daa25e16943bdc71d9696f0e581349e
francisco <franmach20@outlook.com>
2024-06-13 22:40:43 -0300

    crud de usuarios y reservas, falta implementar la edicion en las tablas actuales -se creo una nueva view para el create -se modifico todo el index +controller -se modifico el create -se añadio un create view -Layout modificado

--- obj/Debug/net8.0/ObligatorioProgram3.AssemblyInfo.cs
+++ obj/Debug/net8.0/ObligatorioProgram3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioProgram3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8e06ab0c15906aa2173a26bc2ec4e329b35b664")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioProgram3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioProgram3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
