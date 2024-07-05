c2e2c5e33829b8f11989046b70a208f4f97aa314
francisco <franmach20@outlook.com>
2024-07-04 21:10:09 -0300

    aaaaaaa

--- obj/Debug/net8.0/ObligatorioProgram3.AssemblyInfo.cs
+++ obj/Debug/net8.0/ObligatorioProgram3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioProgram3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1af5c570e81e1a310a6c617dff02fa946eb88cd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioProgram3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioProgram3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
