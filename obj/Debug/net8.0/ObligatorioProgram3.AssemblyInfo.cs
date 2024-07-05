fe6e7ae52af0750bc5cb48c838c9d89273804472
angelmachado07 <angelmachadosouza@gmail.com>
2024-07-05 01:38:42 -0300

    coso pum

--- obj/Debug/net8.0/ObligatorioProgram3.AssemblyInfo.cs
+++ obj/Debug/net8.0/ObligatorioProgram3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioProgram3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a857b0a7a9fdb12bd9cd158bd4e1f4444e90f5be")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioProgram3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioProgram3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
