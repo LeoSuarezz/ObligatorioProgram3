4825e59cd6bc0a22defdd8b547039c83a9f21f13
francisco <franmach20@outlook.com>
2024-07-08 22:36:46 -0300

    ahora si

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,18 +26,23 @@ namespace ObligatorioProgram3.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var menuItems = await _context.Menus.ToListAsync();
+            var menuItems = await _context.Menus.Where(m => m.Categoria != "Bebidas").ToListAsync();
+            var reseñas = await _context.Reseñas.ToListAsync();
 
             var claims = User.Claims.Select(c => new { c.Type, c.Value }).ToList();
-            // Verifica los claims aquí para asegurarte de que el usuario tiene el claim "Permisos" con los valores adecuados
-           
-            var categorias = new List<string> { "Entradas","Principal", "Postres" };
+            // Verifica los claims aqu? para asegurarte de que el usuario tiene el claim "Permisos" con los valores adecuados
+
+            var categorias = new List<string> { "Entradas", "Principal", "Postres" };
+
+            ViewBag.Reseñas = reseñas;
 
             ViewBag.Categorias = categorias;
             ViewBag.MenuItems = menuItems;
-            return View();
+
+            return View(menuItems);
 
         }
+
 
         public IActionResult Privacy()
         {
--- Models/Menu.cs
+++ Models/Menu.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace ObligatorioProgram3.Models;
 
 public partial class Menu
 {
+
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre del plato es requerido.")]
     public string NombrePlato { get; set; } = null!;
 
+    [Required(ErrorMessage = "La descripción del plato es requerida.")]
     public string Descripcion { get; set; } = null!;
 
+    [Required(ErrorMessage = "El precio del plato es requerido.")]
     public decimal Precio { get; set; }
 
     public string? RutaImagen { get; set; }
@@ -18,4 +24,5 @@ public partial class Menu
     public string? Categoria { get; set; }
 
     public virtual ICollection<OrdenDetalle> OrdenDetalles { get; set; } = new List<OrdenDetalle>();
+
 }
